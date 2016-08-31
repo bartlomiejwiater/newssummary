16626796e609ffb5815da7200f5291fa7b00feee
Bartek <wiaterb@gmail.com>
2016-08-31 19:28:32 +0200

    first attempt to model schema

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from django.contrib import admin
+from .models import Word, Link, Occurence, Rate
 
-# Register your models here.
+
+admin.register(Word)
+admin.register(Link)
+admin.register(Occurence)
+admin.register(Rate)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
 
-# Create your models here.
+
+class Word(models.Model):
+    name = models.CharField(max_length=50, unique=True, null=False)
+
+
+class Link(models.Model):
+    address = models.URLField(unique=True, null=False)
+    title = models.TextField(null=False)
+    words = models.ManyToManyField(to=Word)
+
+    class Meta:
+        unique_together = (('address', 'title'),)
+
+class Occurence(models.Model):
+    timestamp = models.DateTimeField(null=False)
+    source = models.CharField(max_length=25, null=False)
+
+    class Meta:
+        unique_together = (('timestamp', 'source'),)
+        get_latest_by = 'occurence'
+
+
+class Rate(models.Model):
+    item = models.ForeignKey(ContentType, null=False)
+    weight = models.IntegerField(default=0, null=False)
+    occurence = models.ForeignKey(to=Occurence, null=False)
+
+    class Meta:
+        unique_together = (('item', 'occurence'),)
