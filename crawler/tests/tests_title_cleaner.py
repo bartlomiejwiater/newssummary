94c449349b68d38c3477c299b7debff2f7639b5f
Bartek <wiaterb@gmail.com>
2016-09-03 18:58:16 +0200

    TitleCleaner matches dots, colons and removes words

--- crawler/tests/tests_title_cleaner.py
+++ crawler/tests/tests_title_cleaner.py
@@ -5,7 +5,7 @@ import pytest
 
 
 @pytest.mark.parametrize("test_input, expected", [
-    ('Elity muszą ze sobą rywalizować', 'Elity muszą rywalizować'),
+    ('Elity muszą ze sobą rywalizować', 'Elity muszą sobą rywalizować'),
     ('Trzęsienie ziemi w USA. Wstrząsy o sile 5,6 w skali Richtera w stanie Oklahoma',
      'Trzęsienie ziemi USA Wstrząsy sile 5,6 skali Richtera stanie Oklahoma'),
     ('Porwano go, żeby uczył dyktatora angielskiego',
@@ -14,6 +14,8 @@ import pytest
      '5-latka wypadła balkonu Złapała ją przechodząca obok kobieta'),
     ('5-latka 15-latka latka -',
      '5-latka 15-latka latka'),
+    ('Od dwóch lat zmniejsza się w Polsce stopa bezrobocia. W II kw. 2016 r., według danych Eurostat i GUS (BAEL), wynosiła 6,2 proc. ',
+     'dwóch zmniejsza Polsce stopa bezrobocia II kw 2016 według danych Eurostat GUS BAEL wynosiła 6,2 proc',)
 
 ])
 def test_titlecleaner_removes_unwanted_chars_and_words(test_input, expected):
--- crawler/title_cleaner.py
+++ crawler/title_cleaner.py
@@ -3,7 +3,7 @@
 import re
 
 unwanted_words = ['a', 'albo', 'ale', 'bo', 'by', 'był', 'co', 'czy', 'dla',
-                  'dla', 'do', 'do', 'dot', 'dot.', 'gdy', 'gdzie', 'i', 'jak',
+                  'dla', 'do', 'do', 'dot', 'dot.', 'go', 'gdy', 'gdzie', 'i', 'jak',
                   'jest', 'jest', 'już', 'lat', 'ma', 'mln', 'mln', 'na', 'na',
                   'nie', 'niż', 'niżej', 'o', 'od', 'ona', 'one', 'ono', 'po',
                   'r', 'r', 'się', 'są', 'tak', 'ten', 'to', 'tys', 'u', 'w',
@@ -20,19 +20,28 @@ class TitleCleaner:
     def clean(self):
         clean_title = self.clean_chars()
         clean_title = self.remove_trailing_spaces(clean_title)
+        clean_title = self.clean_words(clean_title)
         return clean_title
 
     def clean_chars(self):
         title_copy = ''.join(self.title)
         for char in unwanted_chars:
-            if '-' == char and '-' in title_copy:
+            if '-' == char and char in title_copy:
                 title_copy = re.sub('(?!<=[\d])-(?!lat)', '', title_copy)
+            elif (',' == char or '.' == char) and char in title_copy:
+                char_match = '\.' if '.' == char else ','
+                title_copy = re.sub('(?!<=[\d]){}(?![\d])'.format(
+                    char_match), '', title_copy)
             else:
                 title_copy = title_copy.replace(char, "")
         return title_copy
 
-    def clean_words(self):
-        pass
+    def clean_words(self, title):
+        titlewords = title.split()
+        resultwords = [word for word in titlewords if word.lower()
+                       not in unwanted_words]
+        result = ' '.join(resultwords)
+        return result
 
     def remove_trailing_spaces(self, title):
         return title.rstrip(' ')
