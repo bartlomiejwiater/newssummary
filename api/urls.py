58ec45f76b08b0700d00c54a9ab6ed914ed0670b
Bartek <wiaterb@gmail.com>
2016-09-05 13:03:43 +0200

    Removed tastypie, installed django rest framework

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from tastypie.api import Api
 from django.conf.urls import url, include
-from api.words import WordResource
-
-
-v1_api = Api(api_name='v1')
-v1_api.register(WordResource())
+from api.words import WordsList
 
 urlpatterns = [
-    url(r'^api/', include(v1_api.urls))
+    url(r'^api/words/', WordsList.as_view())
 ]
--- api/words.py
+++ api/words.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from tastypie.resources import ModelResource
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
+
+from django.db.models import Sum
+from django.utils.timesince import datetime
 from core.models import Word, Occurence
 
 
-class WordResource(ModelResource):
+class WordsList(APIView):
 
-    class Meta:
-        queryset = Word.objects.all()
-        resource_name = 'words'
+    def get(self, request, format=None):
+        """
+        Return a list of all words.
+        """
+        words = [{'name': word.name, 'id': word.id, 'weight': word.weight}
+                 for word in Word.objects.all()
+                 # .filter(rate__occurence__timestamp__date=datetime.datetime.today())
+                 .annotate(weight=Sum('rate__weight')).order_by('-weight')[60:]]
+        return Response(words)
