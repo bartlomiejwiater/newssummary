9d0acb6da1b41d87ad021e796faabedd127bc135
Bartek <wiaterb@gmail.com>
2016-09-05 13:04:34 +0200

    installed django debug toolbar

--- newssummary/urls.py
+++ newssummary/urls.py
@@ -2,9 +2,14 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
 from api.urls import urlpatterns as api_urlspatter
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('api.urls')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [url(r'^__debug__/', include(debug_toolbar.urls))]
--- settings/developing.py
+++ settings/developing.py
@@ -2,7 +2,26 @@
 # -*- coding: utf-8 -*-
 from settings.base import *
 
+DEBUG_TOOLBAR_PATCH_SETTINGS = False
+
 DEBUG = True
+
+INSTALLED_APPS += [
+    'debug_toolbar',
+]
+
+MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+INTERNAL_IPS = ('127.0.0.1',)
 
 DATABASES = {
     'default': {
