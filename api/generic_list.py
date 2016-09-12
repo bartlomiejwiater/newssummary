db124167e9c2c97f8078e83dc665c843e014e006
Bartek <wiaterb@gmail.com>
2016-09-12 22:03:07 +0200

    moved filters to another function

--- api/generic_list.py
+++ api/generic_list.py
@@ -4,7 +4,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from django.db.models import Sum
-from django.utils.timesince import datetime
 from core.models import Word, Link
 
 
@@ -16,42 +15,53 @@ class GenericList(APIView):
         """
         Return a list of selected entry.
         """
-        startdate, enddate = None, None
-        source = None
+        filter_kwargs = {}
 
         if 'startdate' in request.GET:
-            startdate = request.GET['startdate']
+            filter_kwargs['startdate'] = request.GET['startdate']
         if 'enddate' in request.GET:
-            enddate = request.GET['enddate']
+            filter_kwargs['enddate'] = request.GET['enddate']
+        if 'source' in request.GET:
+            filter_kwargs['source'] = request.GET.getlist('source')
 
         if self.list_class == Word:
             entries = self.list_class.objects.all().values('id', 'name')
         elif self.list_class == Link:
             entries = self.list_class.objects.all().values('id', 'title', 'address')
+        else:
+            raise NotImplementedError
 
         if 'words' in request.GET and self.list_class == Link:
             wanted_words = request.GET.getlist('words')
             entries = self.fiter_links_according_to_word(entries, wanted_words)
 
-        if startdate and enddate:
-            entries = entries.filter(
-                rate__occurence__timestamp__date__range=(startdate, enddate))
-        elif startdate and enddate is None:
-            entries = entries.filter(
-                rate__occurence__timestamp__date__gte=startdate)
-        elif startdate is None and enddate:
-            entries = entries.filter(
-                rate__occurence__timestamp__date__lte=enddate)
-
-        if 'source' in request.GET:
-            source = request.GET.getlist('source')
-            entries = entries.filter(rate__occurence__source__in=source)
-
+        entries = self.filter(entries, **filter_kwargs)
         entries = entries.annotate(
             weight=Sum('rate__weight')).order_by('-weight')[:60]
-
         results = {'results': entries}
+
         return Response(results)
+
+    def filter(self, queryset, **kwargs):
+        startdate = kwargs.get('startdate', None)
+        enddate = kwargs.get('enddate', None)
+
+        if startdate and enddate:
+            queryset = queryset.filter(
+                rate__occurence__timestamp__date__range=(startdate, enddate))
+        elif startdate and enddate is None:
+            queryset = queryset.filter(
+                rate__occurence__timestamp__date__gte=startdate)
+        elif startdate is None and enddate:
+            queryset = queryset.filter(
+                rate__occurence__timestamp__date__lte=enddate)
+
+        source = kwargs.get('source', None)
+
+        if source:
+            queryset = queryset.filter(rate__occurence__source__in=source)
+
+        return queryset
 
     def fiter_links_according_to_word(self, wanted_words):
         raise NotImplementedError
