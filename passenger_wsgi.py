f345f60c519802f7247626a621c7665c406a262b
Bartek <wiaterb@gmail.com>
2016-09-12 17:52:21 +0200

    set to development env

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,7 +1,9 @@
-import sys, os
- 
+import sys
+import os
+import os
+
 sys.path.append(os.getcwd())
-os.environ['DJANGO_SETTINGS_MODULE'] = "settings.production" 
- 
+os.environ['DJANGO_SETTINGS_MODULE'] = "settings.developing"
+
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
