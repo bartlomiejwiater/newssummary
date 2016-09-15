2cc02137fd810d356f7b0435e4dd119b554383c2
Bartek <wiaterb@gmail.com>
2016-09-15 22:08:37 +0200

    submit form by ajax

--- stats/forms/words_form.py
+++ stats/forms/words_form.py
@@ -6,22 +6,27 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 
 
+class ChoiceFieldNoValidation(forms.MultipleChoiceField):
+
+    def validate(self, value):
+        pass
+
+
 class WordsForm(forms.Form):
 
     def __init__(self, *args, **kwargs):
         super(WordsForm, self).__init__(*args, **kwargs)
-        self.helper = FormHelper()
 
-        self.helper.form_id = 'id-exampleForm'
+        self.helper = FormHelper()
+        self.helper.form_id = 'id-get-words-list'
         self.helper.form_class = 'blueForms form-horizontal'
         self.helper.form_method = 'get'
-        self.helper.form_action = 'submit_survey'
         self.helper.label_class = 'col-sm-2'
         self.helper.field_class = 'col-sm-10'
 
         self.helper.add_input(Submit('submit', 'Submit'))
 
-    my_choice = forms.MultipleChoiceField(
+    words = ChoiceFieldNoValidation(
         widget=HeavySelect2MultipleWidget(data_view='words-select'),
         required=False)
     start_date = forms.DateTimeField(input_formats='%Y-%m-%d', required=False)
--- stats/views.py
+++ stats/views.py
@@ -7,11 +7,31 @@ from stats.forms.words_form import WordsForm
 
 
 def words(request):
+    print('words - view')
     domain = Site.objects.get_current().domain
     url = 'http://' + domain + reverse('words-list')
-    response = requests.get(url)
+    print(url)
+
+    if not request.GET:
+        print('not request.GET:')
+        form = WordsForm(data=request.POST)
+        response = requests.get(url)
+        words_to_response = response.json()['results']
+        return render(request, 'stats/words.html', {'words': words_to_response,
+                                                    'form_element': form})
+    else:
+        print('request.GET:')
+
+    form = WordsForm(data=request.GET)
+
+    if form.is_valid():
+        print('form is valid')
+        response = requests.get(url)
+    else:
+        print('form is not valid')
+        response = requests.get(url)
 
     words_to_response = response.json()['results']
-    form = WordsForm()
+
     return render(request, 'stats/words.html', {'words': words_to_response,
                                                 'form_element': form})
