d68016806e39f8cb64a3ba67b94c274e5264cf2c
Bartek <wiaterb@gmail.com>
2016-09-24 20:41:46 +0200

    Refactored views.py

--- stats/views.py
+++ stats/views.py
@@ -7,62 +7,23 @@ from utils.build_url import build_url
 
 
 def words(request):
-    # print('words - view')
-    # domain = Site.objects.get_current().domain
-    # domain = request.META['HTTP_HOST']
-    # print('domain')
-    # print(domain)
-    # print(request.META['HTTP_HOST'])
-    # url = 'http://' + domain + reverse('words-list')
     url = reverse('words-list')
-    print(url)
-    # c = Client()
 
-    if not request.GET:
-        # print('not request.GET:')
-        # response = requests.get(url)
-        response = Client().get(url)
-        # print(response)
-        # print(response.json())
-        words_to_response = response.json()['results']
-        # print(words_to_response)
+    if request.GET:
+        form = WordsForm(data=request.GET)
 
-        form = WordsForm()
-        return render(request, 'stats/words.html', {'words': words_to_response,
-                                                    'form_element': form})
-    # else:
-    #     print('request.GET:')
+        if form.is_valid():
+            params = {}
+            for par in [('start_date', 'startdate'), ('end_date', 'enddate')]:
+                p = request.GET.get(par[0], None)
+                if p:
+                    params[par[1]] = p
 
-    form = WordsForm(data=request.GET)
-
-    if form.is_valid():
-        print('form is valid')
-        params = {}
-        # if 'start_date' in request.GET:
-        #     startdate = request.GET.get('start_date', None)
-        #     if startdate:
-        #         params['startdate'] = startdate
-        # if 'end_date' in request.GET:
-        #     enddate = request.GET.get('end_date', None)
-        #     if enddate:
-        #         params['enddate'] = enddate
-
-        for param in [('start_date', 'startdate'), ('end_date', 'enddate')]:
-            p = request.GET.get(param[0], None)
-            if p:
-                params[param[1]] = p
-        # params['get'] = params
-        # print(params)
-        url = build_url('words-list', get=params)
-        # print(url)
-        # response = requests.get(url, params=params)
-        response = Client().get(url)
+            url = build_url('words-list', get=params)
     else:
-        # print('form is not valid')
-        # c = Client()
-        response = Client().get(url)
-        # response = requests.get(url)
+        form = WordsForm()
 
+    response = Client().get(url)
     words_to_response = response.json()['results']
 
     return render(request, 'stats/words.html', {'words': words_to_response,
