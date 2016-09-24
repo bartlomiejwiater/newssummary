5c06838ca25775281080c704079cee3de1bd873c
Bartek <wiaterb@gmail.com>
2016-09-24 19:40:10 +0200

    Removed multiselect words field

--- stats/forms/words_form.py
+++ stats/forms/words_form.py
@@ -1,15 +1,6 @@
 from django import forms
-from django.contrib.sites.models import Site
-from django_select2.forms import HeavySelect2MultipleWidget, HeavySelect2TagWidget
-from django.urls import reverse
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
-
-
-class ChoiceFieldNoValidation(forms.MultipleChoiceField):
-
-    def validate(self, value):
-        pass
 
 
 class WordsForm(forms.Form):
@@ -20,14 +11,11 @@ class WordsForm(forms.Form):
         self.helper = FormHelper()
         self.helper.form_id = 'id-get-words-list'
         self.helper.form_class = 'blueForms form-horizontal'
-        self.helper.form_method = 'get'
+        self.helper.form_method = 'GET'
         self.helper.label_class = 'col-sm-2'
         self.helper.field_class = 'col-sm-10'
 
         self.helper.add_input(Submit('submit', 'Submit'))
 
-    words = ChoiceFieldNoValidation(
-        widget=HeavySelect2MultipleWidget(data_view='words-select'),
-        required=False)
-    start_date = forms.DateTimeField(input_formats='%Y-%m-%d', required=False)
-    end_date = forms.DateTimeField(input_formats='%Y-%m-%d', required=False)
+    start_date = forms.DateTimeField(required=False)
+    end_date = forms.DateTimeField(required=False)
